b860eeb7f8af0aec7c268a50f61ee257259062bc
Vladyslav <112310301+VladyslavMinaev@users.noreply.github.com>
2023-04-04 23:59:17 +0300

    Add files via upload

---
+++ Home_task_3/SecondTaskStrings/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strings
+{
+    internal class Program
+    {
+        static void SecondSub(string text, string sub)
+        {
+            int firstIndex = text.IndexOf(sub);
+            if (firstIndex == -1)
+            {
+                Console.WriteLine("Substring not found");
+            }
+            else
+            {
+                int secondIndex = text.IndexOf(sub, firstIndex + 1);
+                if (secondIndex == -1)
+                {
+                    Console.WriteLine("Second substring not found");
+                }
+                else
+                {
+                    Console.WriteLine("Second substring index: " + secondIndex);
+                }
+            }
+        }
+        static int CountWordsStartingWithUpperCase(string text)
+        {
+            int wordCount = 0;
+            bool newWord = true; // flag indicating the start of a new word
+            foreach (char c in text)
+            {
+                if (Char.IsLetter(c))
+                { // if the character is a letter
+                    if (newWord && Char.IsUpper(c))
+                    { // if it's the start of a new word and the letter is uppercase
+                        wordCount++;
+                    }
+                    newWord = false;
+                }
+                else
+                { // if the character is not a letter
+                    newWord = true;
+                }
+            }
+            return wordCount;
+        }
+
+        
+        static void Main(string[] args)
+        {
+            //1 task
+            Console.WriteLine("================Task One================");
+            Console.Write("Enter text here: ");
+            string text1 = Console.ReadLine();
+            Console.Write("Enter substring: ");
+            string substring = Console.ReadLine();
+
+            SecondSub(text1, substring);
+
+            //2 task
+            Console.WriteLine("================Task Two================");
+            Console.Write("Enter the text: ");
+            string text2 = Console.ReadLine();
+
+            int wordCount = CountWordsStartingWithUpperCase(text2);
+            Console.WriteLine("Number of words starting with an uppercase letter: " + wordCount);
+
+            Console.ReadLine();
+
+            //3 task
+            Console.WriteLine("================Task Three================");
+            Console.Write("Enter the text: ");
+            string text3 = Console.ReadLine();
+
+            Console.Write("Enter the replacement string for words with double letters: ");
+            string replacement = Console.ReadLine();
+
+            Task3 task3 = new Task3(text3);
+
+            string result = task3.ReplaceWordsWithDoubleLetters(replacement);
+            Console.WriteLine("Result of replacement: " + result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Home_task_3/SecondTaskStrings/Task3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strings
+{
+    public class Task3
+    {
+        public string Text { get; private set; }
+
+        public Task3(string text)
+        {
+            Text = text;
+        }
+        public Task3()
+        {
+            Text = "";
+
+        }
+        public void SetText(string text)=>Text = text;
+        public string ReplaceWordsWithDoubleLetters(string replacement)
+        {
+            string[] words = Text.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (HasDoubleLetter(words[i]))
+                {
+                    words[i] = replacement;
+                }
+            }
+
+            return string.Join(" ", words);
+        }
+
+        static bool HasDoubleLetter(string word)
+        {
+            for (int i = 0; i < word.Length - 1; i++)
+            {
+                if (word[i] == word[i + 1])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
