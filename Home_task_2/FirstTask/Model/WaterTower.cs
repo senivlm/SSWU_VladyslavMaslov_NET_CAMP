21dc1cb3d03073fef391bbbfcb3ec9eea312f602
senivlm <91743906+senivlm@users.noreply.github.com>
2023-04-18 19:36:21 +0300

    Update WaterTower.cs

--- Home_task_2/FirstTask/Model/WaterTower.cs
+++ Home_task_2/FirstTask/Model/WaterTower.cs
@@ -5,9 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace task.Model
-{
+{// А як забрати воду користувачеві?
     public class WaterTower
-    {
+    {// властивості з великої літери.
         public int maxVolume { get; private set; }
         public int currentVolume { get; private set; }
         //public bool isPumpOn { get;private set; }
